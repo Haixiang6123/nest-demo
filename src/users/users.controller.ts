8dc84e5348422368ccfd565abd496e2475b6c45d
haixiangyan <haixiang6123@gmail.com>
2021-07-05 07:42:16 +0800

    feat: 添加 Swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,29 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @ApiOkResponse({ type: User, isArray: true })
   @Get()
-  getUsers(): any {
-    return ['Tom', 'Jerry'];
+  getUsers(): User[] {
+    return this.usersService.findAll();
   }
 
+  @ApiOkResponse({ type: User, description: 'Get the user by his id' })
   @Get(':id')
-  getUserById(@Param('id') id: string): any {
-    return {
-      id,
-    };
+  getUserById(@Param('id') id: string): User {
+    return this.usersService.findUserById(Number(id));
+  }
+
+  @ApiCreatedResponse({ type: User })
+  @Post()
+  createUser(@Body() body: CreateUserDto): User {
+    return this.usersService.createUser(body);
   }
 }
