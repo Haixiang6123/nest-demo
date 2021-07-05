b6b86a62b5dff0345e88e2cb1bdea90ccd2fa747
haixiangyan <haixiang6123@gmail.com>
2021-07-05 08:52:12 +0800

    feat: 添加 db 配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TodosModule } from './todos/todos.module';
+import config from '../ormconfig';
 
 @Module({
-  imports: [UsersModule, TodosModule],
+  imports: [UsersModule, TodosModule, TypeOrmModule.forRoot(config)],
   controllers: [AppController],
   providers: [AppService],
 })
