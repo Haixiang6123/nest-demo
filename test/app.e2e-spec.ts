a08c38873e26ab4a80c99571727a4d779cca33bb
haixiangyan <haixiang6123@gmail.com>
2021-07-08 09:30:14 +0800

    feat: 添加 e2e 测试

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Client } from '../src/client.entity';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
+  const mockClient = {};
+
+  const mockClientRepository = {
+    findOneOrFail: jest.fn().mockResolvedValue(mockClient),
+  };
+
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(getRepositoryToken(Client))
+      .useValue(mockClientRepository)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/app/hello (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/app/hello')
       .expect(200)
-      .expect('Hello World!');
+      .expect(mockClient);
   });
 });
