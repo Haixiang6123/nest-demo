32b4c25fa1adcdbf3b55c3407bf9627282d66007
haixiangyan <haixiang6123@gmail.com>
2021-07-08 09:04:12 +0800

    feat: 添加 controller 测试

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockUserService = {
+    createUser: jest.fn((dto) => ({
+      id: Date.now(),
+      ...dto,
+    })),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+      providers: [UsersService],
+    })
+      .overrideProvider(UsersService)
+      .useValue(mockUserService)
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a user', () => {
+    const dto = { name: 'Jack' };
+
+    expect(controller.createUser(dto)).toEqual({
+      id: expect.any(Number),
+      ...dto,
+    });
+
+    expect(mockUserService.createUser).toHaveBeenCalledWith(dto);
   });
 });
